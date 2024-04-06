af716136d8970b8cbdaae1d93d3cb234d4c55056
Brian Staruk <brian@staruk.net>
2024-04-06 07:51:51 -0400

    style tweaks

--- app1/src/components/HomePage/wrappers.ts
+++ app1/src/components/HomePage/wrappers.ts
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  row-gap: 2rem;
   align-items: center;
   justify-content: center;
-  row-gap: 1rem;
   width: 100%;
   min-height: 100vh;
   padding: 2rem;
