06ab4fbf4e9b8d6f2c7cd77505f82712ad9457de
Brian Staruk <brian@staruk.net>
2024-04-06 07:37:47 -0400

    adding federated component to app1

--- app1/src/components/HomePage/index.tsx
+++ app1/src/components/HomePage/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Wrapper, TitleWrapper } from './wrappers';
 
+const CardAlpha = React.lazy(() => import('micro1/CardAlpha'));
+
 const HomePage = () => {
   return (
     <Wrapper>
       <TitleWrapper>Hello World</TitleWrapper>
+      <React.Suspense fallback={<p>Loading...</p>}>
+        <CardAlpha />
+      </React.Suspense>
     </Wrapper>
   );
 };
---
+++ app1/src/remoteTypes.d.ts
@@ -0,0 +1,7 @@
+///<reference types="react" />
+
+declare module 'micro1/CardAlpha' {
+  const CardAlpha: React.ComponentType;
+
+  export default CardAlpha;
+}
