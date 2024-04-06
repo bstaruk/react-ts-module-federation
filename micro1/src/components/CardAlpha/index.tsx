80c68509015afe1e7fdb2b6c42031c7f469ce8ec
Brian Staruk <brian@staruk.net>
2024-04-06 07:29:34 -0400

    making title optional (for now)

--- micro1/src/components/CardAlpha/index.tsx
+++ micro1/src/components/CardAlpha/index.tsx
@@ -2,10 +2,10 @@ import React, { ReactNode } from 'react';
 import { Wrapper, Title } from './wrappers';
 
 type CardAlphaProps = {
-  title: string;
+  title?: string;
 };
 
-const CardAlpha = ({ title }: CardAlphaProps): ReactNode => (
+const CardAlpha = ({ title = 'Hello World' }: CardAlphaProps): ReactNode => (
   <Wrapper>
     <Title>{title}</Title>
   </Wrapper>
