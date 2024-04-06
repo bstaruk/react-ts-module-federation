2b55f568df680f0dafb20d2bb973feee3f25a3f4
Brian Staruk <brian@staruk.net>
2024-04-06 09:27:57 -0400

    adding support for children, style tweaks

--- micro1/src/components/CardAlpha/index.tsx
+++ micro1/src/components/CardAlpha/index.tsx
@@ -2,12 +2,17 @@ import React, { ReactNode } from 'react';
 import { Wrapper, Title } from './wrappers';
 
 type CardAlphaProps = {
-  title?: string;
+  children?: ReactNode;
+  title: string;
 };
 
-const CardAlpha = ({ title = 'Hello World' }: CardAlphaProps): ReactNode => (
+const CardAlpha = ({
+  children,
+  title = 'Hello World',
+}: CardAlphaProps): ReactNode => (
   <Wrapper>
     <Title>{title}</Title>
+    {children}
   </Wrapper>
 );
 
--- micro1/src/components/CardAlpha/wrappers.ts
+++ micro1/src/components/CardAlpha/wrappers.ts
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.section`
+  width: 100%;
   display: flex;
   flex-direction: column;
   column-gap: 1rem;
-  background-color: ${(p) => p.theme.colors.wine};
+  border: 1px solid ${(p) => p.theme.colors.wine};
   padding: 1.5rem;
   border-radius: 0.375rem;
 `;
 
 export const Title = styled.h3`
-  color: ${(p) => p.theme.colors.white};
+  color: ${(p) => p.theme.colors.black};
   font-size: 1.5rem;
-  font-weight: bold;
+  letter-spacing: 0.03em;
+  font-weight: 700;
 `;
